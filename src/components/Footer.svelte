37c0e610459520c7966473b418d9906db46e4fe0
cuvar <luca-mueller@gmx.net>
2023-09-24 21:39:48 +0200

    fix: non-working cookie button

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,14 +5,8 @@
 <footer
   class="flex w-full items-center justify-center space-x-4 bg-slate-900 py-6 text-neutral-200 absolute bottom-0 left-0"
 >
-  <button
-    type="button"
-    class="text-sm hover:text-teal-300"
-    onClick={() => {
-      window.CC.showSettings(0); // 0 steht für den Delay, nachdem das Popup aufpoppt
-    }}
+  <button type="button" class="text-sm hover:text-teal-300" data-cc="c-settings"
+    >Cookies</button
   >
-    Cookies
-  </button>
   <Link href="/imprint" text={"Imprint"} />
 </footer>
