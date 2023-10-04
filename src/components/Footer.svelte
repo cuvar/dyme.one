4802ad0d1ad9ef4d80bec4f504b6322a715d4cf2
cuvar <luca-mueller@gmx.net>
2023-10-04 15:52:27 +0200

    feat: add imprint

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer
-  class="flex w-full items-center justify-center space-x-4 bg-slate-900 py-6 text-neutral-200 absolute bottom-0 left-0"
+  class="flex w-full items-center justify-center space-x-4 py-6 text-neutral-200 absolute bottom-0 left-0"
 >
   <button type="button" class="text-sm hover:text-teal-300" data-cc="c-settings"
     >Cookies</button
